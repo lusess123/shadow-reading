3bb1932ac6cbb0d37f648f356342283244842222
lusess123 <zyk2003xxx@163.com>
2024-07-29 17:34:39 +0800

    refactor(build): remove duplicated docker build command
    Removed unnecessary duplicate docker build command from the build.sh script.
    This change simplifies the build process by eliminating redundant steps,
    resulting in a cleaner and more maintainable script.

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 # shellcheck disable=SC2148
 
+
+
+docker build -t shadow-reading  .
+
 containers=$(docker ps -a | grep shadow-reading | awk '{print $1}')
 if [ -n "$containers" ]; then
   echo "$containers" | xargs docker rm -f
 fi
-
-docker build -t shadow-reading  .
 
 # shellcheck disable=SC2046
 docker run -d  -p 3050:3000  -v $(pwd)/audio_cache:/app/audio_cache shadow-reading
