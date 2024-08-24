aa74fa01e178d0c694ed6ff3342468cd5fc3eb0a
lusess123 <zyk2003xxx@163.com>
2024-08-24 22:24:58 +0800

    ``` feat(build): remove redundant container cleanup code
    The container cleanup logic that removes stopped containers named 'shadow-reading' has
    been commented out. Additionally, a new container is launched with specific settings
    after the build process. This change streamlines the build script by removing
    unnecessary steps and ensures an updated container is available following the build.
    ```

--- build.sh
+++ build.sh
@@ -4,10 +4,12 @@
 
 docker build -t shadow-reading  .
 
+
+
+# shellcheck disable=SC2046
+docker run -d  -p 3050:3000  -v $(pwd)/audio_cache:/app/audio_cache shadow-reading
+
 containers=$(docker ps -a | grep shadow-reading | awk '{print $1}')
 if [ -n "$containers" ]; then
   echo "$containers" | xargs docker rm -f
 fi
-
-# shellcheck disable=SC2046
-docker run -d  -p 3050:3000  -v $(pwd)/audio_cache:/app/audio_cache shadow-reading
