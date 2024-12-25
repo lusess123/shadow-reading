7030723f7cba8a9a42179bc5b97e818028c40cf8
lusess123 <zyk2003xxx@163.com>
2024-12-25 20:03:48 +0800

    feat(app): add new sentences to the bz source
    - Introduce a new list (list7) with additional sentences
    - Update the export default statement to include list7

--- src/app/source/bz.ts
+++ src/app/source/bz.ts
@@ -368,9 +368,90 @@ const list2 = [
       "phonetic": "[ænd duː juː fiːl ˈstrɒŋɡə? nəʊ.]"
     }
   ]
+
+  const list7 = [
+    {
+      "sentence": "Simone and I used to be very close, back when I was her hairdresser.",
+      "original": "Simone and I used to be very close, back when I was her hairdresser.",
+      "translation": "西蒙娜和我曾经非常亲密，那时我是她的发型师。",
+      "phonetic": "[sɪˈmoʊn ənd aɪ juːst tə bi ˈvɛri kloʊs, bæk wɛn aɪ wəz hɜr ˈhɛrˌdrɛsər.]"
+    },
+    {
+      "sentence": "Taylor and I were forced to socialize, because I was her husband's agent.",
+      "original": "Taylor and I were forced to socialize, because I was her husband's agent.",
+      "translation": "泰勒和我被迫社交，因为我是她丈夫的经纪人。",
+      "phonetic": "[ˈteɪlər ənd aɪ wɜr fɔrst tə ˈsoʊʃəlaɪz, bɪˈkəz aɪ wəz hɜr ˈhʌzbəndz ˈeɪʤənt.]"
+    },
+    {
+      "sentence": "I didn't know Beth Ahn at all, even though she was a neighbor.",
+      "original": "I didn't know Beth Ahn at all, even though she was a neighbor.",
+      "translation": "我完全不认识贝丝·安，尽管她是我的邻居。",
+      "phonetic": "[aɪ ˈdɪdənt noʊ bɛθ ɑːn ət ɔl, ˈivən ðoʊ ʃi wəz ə ˈneɪbər.]"
+    },
+    {
+      "sentence": "I used to gossip with her for hours. She adored me.",
+      "original": "I used to gossip with her for hours. She adored me.",
+      "translation": "我过去常和她八卦几个小时。她很崇拜我。",
+      "phonetic": "[aɪ juːst tə ˈɡɑsəp wɪð hɜr fɔr ˈaʊərz. ʃi əˈdɔrd mi.]"
+    },
+    {
+      "sentence": "I tried to talk to her at parties, she never got my jokes.",
+      "original": "I tried to talk to her at parties, she never got my jokes.",
+      "translation": "我试着在聚会上和她说话，但她从来不懂我的笑话。",
+      "phonetic": "[aɪ traɪd tə tɔk tə hɜr æt ˈpɑrtiz, ʃi ˈnɛvər ɡɑt maɪ ʤoʊks.]"
+    },
+    {
+      "sentence": "I used to watch her at the market, she seemed nice.",
+      "original": "I used to watch her at the market, she seemed nice.",
+      "translation": "我过去常在市场上看着她，她看起来很友好。",
+      "phonetic": "[aɪ juːst tə wɑʧ hɜr æt ðə ˈmɑrkɪt, ʃi siːmd naɪs.]"
+    },
+    {
+      "sentence": "It's funny about people.",
+      "original": "It's funny about people.",
+      "translation": "人真的很有趣。",
+      "phonetic": "[ɪts ˈfʌni əˈbaʊt ˈpiːpl.]"
+    },
+    {
+      "sentence": "They come into our lives every day.",
+      "original": "They come into our lives every day.",
+      "translation": "他们每天都会进入我们的生活。",
+      "phonetic": "[ðeɪ kʌm ˈɪntu ˈaʊər ˈlaɪvz ˈɛvri deɪ.]"
+    },
+    {
+      "sentence": "And you never know who will play the most important part.",
+      "original": "And you never know who will play the most important part.",
+      "translation": "你永远不知道谁会扮演最重要的角色。",
+      "phonetic": "[ænd ju ˈnɛvər noʊ huː wɪl pleɪ ðə ˈmoʊst ˌɪmˈpɔrtənt pɑrt.]"
+    },
+    {
+      "sentence": "A total stranger might be the one to save you or destroy you.",
+      "original": "A total stranger might be the one to save you or destroy you.",
+      "translation": "一个完全陌生的人可能是拯救你或毁灭你的人。",
+      "phonetic": "[ə ˈtoʊtəl ˈstreɪnʤər maɪt bi ðə wʌn tə seɪv ju ɔr dɪˈstrɔɪ ju.]"
+    },
+    {
+      "sentence": "To be honest, I feel responsible for his death.",
+      "original": "To be honest, I feel responsible for his death.",
+      "translation": "老实说，我觉得对他的死亡负有责任。",
+      "phonetic": "[tə bi ˈɑːnɪst, aɪ fiːl rɪˈspɑnsəbəl fɔr hɪz dɛθ.]"
+    },
+    {
+      "sentence": "I'm not sure anyone would've died for if it wasn't for me.",
+      "original": "I'm not sure anyone would've died for if it wasn't for me.",
+      "translation": "我不确定如果不是因为我，是否会有人因此死亡。",
+      "phonetic": "[aɪm nɑt ʃʊr ˈɛniˌwʌn ˈwʊdəv daɪd fɔr ɪf ɪt ˈwʌzənt fɔr mi.]"
+    },
+    {
+      "sentence": "I knew she was going to kill him and I'm so glad she did.",
+      "original": "I knew she was going to kill him and I'm so glad she did.",
+      "translation": "我知道她会杀了他，我为此感到很高兴。",
+      "phonetic": "[aɪ nu ʃi wəz ˈɡoʊɪŋ tə kɪl hɪm ənd aɪm soʊ ɡlæd ʃi dɪd.]"
+    }
+  ]
   
   
   
 
-  export default list1.concat(list2).concat(list3).concat(list4).concat(list5).concat(list6);
+  export default list1.concat(list2).concat(list3).concat(list4).concat(list5).concat(list6).concat(list7);
   
