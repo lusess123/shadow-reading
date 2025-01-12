8d41a1cc4550d1ad9da0799732a8f5c94a5b0bfd
lusess123 <zyk2003xxx@163.com>
2025-01-12 12:02:53 +0800

    refactor(app): consolidate song lyrics and optimize data structure
    - Merge duplicate lyrics into single entries for "Blowin' in the Wind" and "Peter Pan"
    - Remove redundant sentences and simplify data structure
    - Update phonetic transcriptions for combined lyrics
    - Delete unused removeDuplicates function and related code

--- src/app/source/500m.ts
+++ src/app/source/500m.ts
@@ -21,46 +21,22 @@ const list = [
 
   const list2 = [
     {
-      "sentence": "How many roads must a man walk down before you can call him a man? How many seas must a white dove sail before she sleeps in the sand?",
-      "original": "How many roads must a man walk down before you can call him a man? How many seas must a white dove sail before she sleeps in the sand?",
-      "translation": "一个人要走多少条路才能被称作‘人’？一只白鸽要飞过多少海洋才能在沙滩上栖息？",
-      "phonetic": "[haʊ ˈmɛni roʊdz mʌst ə mæn wɔːk daʊn bɪˈfɔːr ju kæn kɔːl hɪm ə mæn? haʊ ˈmɛni siːz mʌst ə waɪt dʌv seɪl bɪˈfɔːr ʃi sliːps ɪn ðə sænd?]"
+      "sentence": "How many roads must a man walk down before you can call him a man? How many seas must a white dove sail before she sleeps in the sand? Yes, how many times must the cannon balls fly before they're forever banned?",
+      "original": "How many roads must a man walk down before you can call him a man? How many seas must a white dove sail before she sleeps in the sand? Yes, how many times must the cannon balls fly before they're forever banned?",
+      "translation": "一个人要走多少条路才能被称作‘人’？一只白鸽要飞过多少海洋才能在沙滩上栖息？是的，大炮的炮弹要飞多少次才能永远被禁止？",
+      "phonetic": "[haʊ ˈmɛni roʊdz mʌst ə mæn wɔːk daʊn bɪˈfɔːr ju kæn kɔːl hɪm ə mæn? haʊ ˈmɛni siːz mʌst ə waɪt dʌv seɪl bɪˈfɔːr ʃi sliːps ɪn ðə sænd? jɛs, haʊ ˈmɛni taɪmz mʌst ðə ˈkænən bɔːlz flaɪ bɪˈfɔːr ðeər fɔːˈɛvər bænd?]"
     },
     {
-      "sentence": "Yes, how many times must the cannon balls fly before they're forever banned?",
-      "original": "Yes, how many times must the cannon balls fly before they're forever banned?",
-      "translation": "是的，大炮的炮弹要飞多少次才能永远被禁止？",
-      "phonetic": "[jɛs, haʊ ˈmɛni taɪmz mʌst ðə ˈkænən bɔːlz flaɪ bɪˈfɔːr ðeər fɔːˈɛvər bænd?]"
+      "sentence": "The answer my friend is blowin' in the wind, the answer is blowin' in the wind. Yes, how many years can a mountain exist before it's washed to the sea? Yes, how many years can some people exist before they're allowed to be free?",
+      "original": "The answer my friend is blowin' in the wind, the answer is blowin' in the wind. Yes, how many years can a mountain exist before it's washed to the sea? Yes, how many years can some people exist before they're allowed to be free?",
+      "translation": "答案，我的朋友，就在风中飘荡，答案就在风中飘荡。是的，一座山能存在多少年才会被冲刷到海里？是的，一些人能存在多少年才能获得自由？",
+      "phonetic": "[ði ˈænsər maɪ frɛnd ɪz ˈbloʊɪn ɪn ðə wɪnd, ði ˈænsər ɪz ˈbloʊɪn ɪn ðə wɪnd. jɛs, haʊ ˈmɛni jɪərz kæn ə ˈmaʊntən ɪɡˈzɪst bɪˈfɔːr ɪts wɒʃt tə ðə siː? jɛs, haʊ ˈmɛni jɪərz kæn sʌm ˈpiːpl ɪɡˈzɪst bɪˈfɔːr ðeər əˈlaʊd tə biː friː?]"
     },
     {
-      "sentence": "The answer my friend is blowin' in the wind, the answer is blowin' in the wind.",
-      "original": "The answer my friend is blowin' in the wind, the answer is blowin' in the wind.",
-      "translation": "答案，我的朋友，就在风中飘荡，答案就在风中飘荡。",
-      "phonetic": "[ði ˈænsər maɪ frɛnd ɪz ˈbloʊɪn ɪn ðə wɪnd, ði ˈænsər ɪz ˈbloʊɪn ɪn ðə wɪnd.]"
-    },
-    {
-      "sentence": "Yes, how many years can a mountain exist before it's washed to the sea? Yes, how many years can some people exist before they're allowed to be free?",
-      "original": "Yes, how many years can a mountain exist before it's washed to the sea? Yes, how many years can some people exist before they're allowed to be free?",
-      "translation": "是的，一座山能存在多少年才会被冲刷到海里？是的，一些人能存在多少年才能获得自由？",
-      "phonetic": "[jɛs, haʊ ˈmɛni jɪərz kæn ə ˈmaʊntən ɪɡˈzɪst bɪˈfɔːr ɪts wɒʃt tə ðə siː? jɛs, haʊ ˈmɛni jɪərz kæn sʌm ˈpiːpl ɪɡˈzɪst bɪˈfɔːr ðeər əˈlaʊd tə biː friː?]"
-    },
-    {
-      "sentence": "Yes, how many times can a man turn his head pretending he just doesn't see?",
-      "original": "Yes, how many times can a man turn his head pretending he just doesn't see?",
-      "translation": "是的，一个人能转多少次头假装看不见？",
-      "phonetic": "[jɛs, haʊ ˈmɛni taɪmz kæn ə mæn tɜːrn hɪz hɛd prɪˈtɛndɪŋ hi dʒʌst ˈdʌznt siː?]"
-    },
-    {
-      "sentence": "The answer my friend is blowin' in the wind, the answer is blowin' in the wind.",
-      "original": "The answer my friend is blowin' in the wind, the answer is blowin' in the wind.",
-      "translation": "答案，我的朋友，就在风中飘荡，答案就在风中飘荡。",
-      "phonetic": "[ði ˈænsər maɪ frɛnd ɪz ˈbloʊɪn ɪn ðə wɪnd, ði ˈænsər ɪz ˈbloʊɪn ɪn ðə wɪnd.]"
-    },
-    {
-      "sentence": "Yes, how many times must a man look up before he can really see the sky? Yes, how many ears must one man have before he can hear people cry?",
-      "original": "Yes, how many times must a man look up before he can really see the sky? Yes, how many ears must one man have before he can hear people cry?",
-      "translation": "是的，一个人要抬多少次头才能真正看到天空？是的，一个人要有多少只耳朵才能听见人们的哭声？",
-      "phonetic": "[jɛs, haʊ ˈmɛni taɪmz mʌst ə mæn lʊk ʌp bɪˈfɔːr hi kæn ˈrɪəli siː ðə skaɪ? jɛs, haʊ ˈmɛni ɪərz mʌst wʌn mæn hæv bɪˈfɔːr hi kæn hɪr ˈpiːpl kraɪ?]"
+      "sentence": "Yes, how many times can a man turn his head pretending he just doesn't see? Yes, how many times must a man look up before he can really see the sky? Yes, how many ears must one man have before he can hear people cry?",
+      "original": "Yes, how many times can a man turn his head pretending he just doesn't see? Yes, how many times must a man look up before he can really see the sky? Yes, how many ears must one man have before he can hear people cry?",
+      "translation": "是的，一个人能转多少次头假装看不见？是的，一个人要抬多少次头才能真正看到天空？是的，一个人要有多少只耳朵才能听见人们的哭声？",
+      "phonetic": "[jɛs, haʊ ˈmɛni taɪmz kæn ə mæn tɜːrn hɪz hɛd prɪˈtɛndɪŋ hi dʒʌst ˈdʌznt siː? jɛs, haʊ ˈmɛni taɪmz mʌst ə mæn lʊk ʌp bɪˈfɔːr hi kæn ˈrɪəli siː ðə skaɪ? jɛs, haʊ ˈmɛni ɪərz mʌst wʌn mæn hæv bɪˈfɔːr hi kæn hɪr ˈpiːpl kraɪ?]"
     },
     {
       "sentence": "Yes, how many deaths will it take till he knows that too many people have died? The answer my friend is blowin' in the wind, the answer is blowin' in the wind.",
@@ -69,6 +45,7 @@ const list = [
       "phonetic": "[jɛs, haʊ ˈmɛni dɛθs wɪl ɪt teɪk tɪl hi noʊz ðæt tuː ˈmɛni ˈpiːpl hæv daɪd? ði ˈænsər maɪ frɛnd ɪz ˈbloʊɪn ɪn ðə wɪnd, ði ˈænsər ɪz ˈbloʊɪn ɪn ðə wɪnd.]"
     }
   ]
+  
   
 
   export default list.concat(list2)
--- src/app/source/peter.ts
+++ src/app/source/peter.ts
@@ -1,186 +1,39 @@
-const list1 = 
-[
+const list1 = [
   {
-    "sentence": "I guess <span style=\"color:red;\">P</span>eter Pan was right, growing up's a waste of time.",
-    "original": "I guess Peter Pan was right, growing up's a waste of time.",
-    "translation": "我觉得彼得·潘没有错，长大成人就是浪费时间。",
-    "phonetic": "[aɪ ɡɛs pɪˈtər pæn wɒz raɪt, ˈɡroʊɪŋ ʌpːz ə weɪst əv taɪm]"
+    "sentence": "I guess Peter Pan was right, growing up's a waste of time. So I think I'll fly away, set a course for brighter days, find the second star, I'm soaring, then straight on to the morning, I know that I'll be fine, ‘Cus I know Peter Pan was right.",
+    "original": "I guess Peter Pan was right, growing up's a waste of time. So I think I'll fly away, set a course for brighter days, find the second star, I'm soaring, then straight on to the morning, I know that I'll be fine, ‘Cus I know Peter Pan was right.",
+    "translation": "我觉得彼得·潘是对的，长大成人就是浪费时间，所以我觉得我会远走高飞，确定路线，奔向美好的日子，寻找第二颗星，我翱翔天际，然后直飞向清晨，我知道我会安然无恙，因为我知道彼得·潘没有错。",
+    "phonetic": "[aɪ ɡɛs pɪˈtər pæn wɒz raɪt, ˈɡroʊɪŋ ʌpːz ə weɪst əv taɪm, soʊ aɪ θɪŋk aɪl flaɪ əˈweɪ, sɛt ə kɔ˂rs fɔ˂r ˈbraɪtər deɪz, faɪnd ðə sɛkənd stɑ˂r, aɪm ˈsɔ˂rɪŋ, ðɛn streɪt ɑ˂n tu˂ ðə ˈmɔ˂rnɪŋ, aɪ noʊ ðæt aɪl bi˂ faɪn, kəz aɪ noʂ ˈpɪtər pæn wɒz raɪt]"
   },
   {
-    "sentence": "So I think I'll <span style=\"color:red;\">f</span>ly away, set a course for brighter days.",
-    "original": "So I think I'll fly away, set a course for brighter days.",
-    "translation": "所以，我觉得我会远走高飞，确定路线，奔向美好的日子。",
-    "phonetic": "[soʊ aɪ θɪŋk aɪl flaɪ əˈweɪ, sɛt ə kɔːrs fɔːr ˈbraɪtər deɪz]"
+    "sentence": "Just a lost boy in a small town, I'm the same kid but I'm grown now, try to make it out, but I don't know how. Wish that I was young, what have I become. Now it's late night and I'm at home, so I make friends with my shadow.",
+    "original": "Just a lost boy in a small town, I'm the same kid but I'm grown now, try to make it out, but I don't know how. Wish that I was young, what have I become. Now it's late night and I'm at home, so I make friends with my shadow.",
+    "translation": "只是个出生在小镇的迷茫男孩，我还是那个孩子，但我已经长大了。想要取得成功，但我不知道怎么做。希望我依然年轻，我变成了什么样子。现在是深夜，我在家，所以我和影子成了朋友。",
+    "phonetic": "[ʤʌst ə lɔ˂st bɔɪ ɪn ə smɔ˂l taʊn, aɪm ðə seɪm kɪd bʌt aɪm ɡroʂn naʂ, traɪ tu˂ meɪk ɪt aʊt, bʌt aɪ doʂnt noʂ haʊ, wɪʃ ðæt aɪ wʌz jʌŋ, wʌt hæv aɪ bɪˈkʌm, naʂ ɪts leɪt naɪt ənd aɪm ət hoʂm, soʂ aɪ meɪk frɛndz wɪð maɪ ˈʃædoʂ]"
   },
   {
-    "sentence": "Find the <span style=\"color:red;\">s</span>econd star, I'm soaring, then straight on to the morning.",
-    "original": "Find the second star, I'm soaring, then straight on to the morning.",
-    "translation": "寻找第二颗星，我翱翔天际，然后直飞向清晨。",
-    "phonetic": "[faɪnd ðə sɛkənd stɑːr, aɪm ˈsɔːrɪŋ, ðɛn streɪt ɑːn tuː ðə ˈmɔːrnɪŋ]"
+    "sentence": "And I play him all my sad, sad songs, and we don't talk. Fairy tales are not the truth, what am I supposed to do. Days feel like a blur now, still feel eighteen but I'm burnt out. So I daydream of what I could be, if I turned back time.",
+    "original": "And I play him all my sad, sad songs, and we don't talk. Fairy tales are not the truth, what am I supposed to do. Days feel like a blur now, still feel eighteen but I'm burnt out. So I daydream of what I could be, if I turned back time.",
+    "translation": "我为他播放所有我悲伤的歌曲，我们却不说话。童话故事不是真的，我应该怎么办。日子现在感觉模糊不清，感觉还是十八岁，但我已精疲力尽。 所以我白日做梦，如果我能让时间倒流，我会是什么样子。",
+    "phonetic": "[ænd aɪ pleɪ hɪm ɔ˂l maɪ sæd, sæd sɔ˂ŋz, ənd wi doʂnt tɔ˂k, ˈfɛəri teɪlz ɑ˂r nɑ˂t ðə tru˂θ, wʌt æm aɪ səˈpoʂzd tu˂ du˂, deɪz fi˂l laɪk ə blɜ˂r naʂ, stɪl fi˂l ˌɛɪˈti˂n bʌt aɪm bɜ˂nt aʊt, soʂ aɪ ˈdeɪdri˂m əv wʌt aɪ kʊd bi˂, ɪf aɪ tɜ˂rnd bæk taɪm]"
   },
   {
-    "sentence": "I know that I'll be fine, ‘Cus I know Peter Pan was right.",
-    "original": "I know that I'll be fine, ‘Cus I know Peter Pan was right.",
-    "translation": "我知道我会安然无恙，因为我知道彼得·潘没有错。",
-    "phonetic": "[aɪ noʊ ðæt aɪl biː faɪn, kəz aɪ noʊ ˈpɪtər pæn wɒz raɪt]"
-  }
-]
-
-const list2 = 
-[
-  {
-    "sentence": "Just a lost <span style=\"color:red;\">b</span>oy in a small town, I'm the same kid but I'm grown now.",
-    "original": "Just a lost boy in a small town, I'm the same kid but I'm grown now.",
-    "translation": "只是个出生在小镇的迷茫男孩，我还是那个孩子，但我已经长大了。",
-    "phonetic": "[ʤʌst ə lɔːst bɔɪ ɪn ə smɔːl taʊn, aɪm ðə seɪm kɪd bʌt aɪm ɡroʊn naʊ]"
-  },
-  {
-    "sentence": "Try to make it out, but I don't know how.",
-    "original": "Try to make it out, but I don't know how.",
-    "translation": "想要取得成功，但我不知道怎么做。",
-    "phonetic": "[traɪ tuː meɪk ɪt aʊt, bʌt aɪ doʊnt noʊ haʊ]"
-  },
-  {
-    "sentence": "Wish that I was young, what have I become.",
-    "original": "Wish that I was young, what have I become.",
-    "translation": "希望我依然年轻，我变成了什么样子。",
-    "phonetic": "[wɪʃ ðæt aɪ wʌz jʌŋ, wʌt hæv aɪ bɪˈkʌm]"
-  },
-  {
-    "sentence": "Now it's late night and I'm at home, so I make friends with my shadow.",
-    "original": "Now it's late night and I'm at home, so I make friends with my shadow.",
-    "translation": "现在是深夜，我在家，所以我和影子成了朋友。",
-    "phonetic": "[naʊ ɪts leɪt naɪt ənd aɪm ət hoʊm, soʊ aɪ meɪk frɛndz wɪð maɪ ˈʃædoʊ]"
-  },
-  {
-    "sentence": "And I play him all my sad, sad songs, and we don't talk.",
-    "original": "And I play him all my sad, sad songs, and we don't talk.",
-    "translation": "我为他播放所有我悲伤的歌曲，我们却不说话。",
-    "phonetic": "[ænd aɪ pleɪ hɪm ɔːl maɪ sæd, sæd sɔːŋz, ənd wi doʊnt tɔːk]"
-  },
-  {
-    "sentence": "Fairy tales are not the truth, what am I supposed to do.",
-    "original": "Fairy tales are not the truth, what am I supposed to do.",
-    "translation": "童话故事不是真的，我应该怎么办。",
-    "phonetic": "[ˈfɛəri teɪlz ɑːr nɑːt ðə truːθ, wʌt æm aɪ səˈpoʊzd tuː duː]"
-  },
-  {
-    "sentence": "I guess Peter Pan was right, growing up's a waste of time.",
-    "original": "I guess Peter Pan was right, growing up's a waste of time.",
-    "translation": "我觉得彼得·潘是对的，长大成人就是浪费时间。",
-    "phonetic": "[aɪ ɡɛs pɪˈtər pæn wɒz raɪt, ˈɡroʊɪŋ ʌpːz ə weɪst əv taɪm]"
-  }
-]
-
-const list3 = 
-[
-  {
-    "sentence": "Days feel like a blur now, still feel eighteen but I'm burnt out.",
-    "original": "Days feel like a blur now, still feel eighteen but I'm burnt out.",
-    "translation": "日子现在感觉模糊不清，感觉还是十八岁，但我已精疲力尽。",
-    "phonetic": "[deɪz fiːl laɪk ə blɜːr naʊ, stɪl fiːl ˌɛɪˈtiːn bʌt aɪm bɜːnt aʊt]"
-  },
-  {
-    "sentence": "So I daydream of what I could be, if I turned back time.",
-    "original": "So I daydream of what I could be, if I turned back time.",
-    "translation": "所以我白日做梦，如果我能让时间倒流，我会是什么样子。",
-    "phonetic": "[soʊ aɪ ˈdeɪdriːm əv wʌt aɪ kʊd biː, ɪf aɪ tɜːrnd bæk taɪm]"
-  },
-  {
-    "sentence": "A couple kids, one girl and a sailor, met a boy in green.",
-    "original": "A couple kids, one girl and a sailor, met a boy in green.",
-    "translation": "几个孩子，一个女孩和一个水手，遇到了一个穿绿色衣服的男孩。",
-    "phonetic": "[ə ˈkʌpl kɪdz, wʌn ɡɜːrl ənd ə ˈseɪlər, mɛt ə bɔɪ ɪn ɡriːn]"
-  },
-  {
-    "sentence": "I thought it'd be me, but I guess that dream wasn't meant to be like.",
-    "original": "I thought it'd be me, but I guess that dream wasn't meant to be like.",
-    "translation": "我以为会是我，但我觉得那个梦并不是注定要这样的。",
-    "phonetic": "[aɪ θɔːt ɪtdə biː miː, bʌt aɪ ɡɛs ðæt driːm ˈwɒzənt mɛnt tuː biː laɪk]"
-  },
-  {
-    "sentence": "Fairy tales are not the truth, what am I supposed to do.",
-    "original": "Fairy tales are not the truth, what am I supposed to do.",
-    "translation": "童话故事不是真的，我应该怎么办。",
-    "phonetic": "[ˈfɛəri teɪlz ɑːr nɑːt ðə truːθ, wʌt æm aɪ səˈpoʊzd tuː duː]"
-  },
-  {
-    "sentence": "I guess Peter Pan was right, and I don't care if I never land.",
-    "original": "I guess Peter Pan was right, and I don't care if I never land.",
-    "translation": "我觉得彼得·潘是对的，我不在乎我是否永远无法着陆。",
-    "phonetic": "[aɪ ɡɛs pɪˈtər pæn wɒz raɪt, ənd aɪ doʊnt kɛər ɪf aɪ ˈnɛvər lænd]"
-  },
-  {
-    "sentence": "Cus' the distant sky's always better than my life right now.",
-    "original": "Cus' the distant sky's always better than my life right now.",
-    "translation": "因为遥远的天空总是比我现在的生活在得好。",
-    "phonetic": "[kəz ðə ˈdɪstənt skaɪz ˈɔːlweɪz ˈbɛtər ðən maɪ laɪf raɪt naʊ]"
+    "sentence": "A couple kids, one girl and a sailor, met a boy in green. I thought it'd be me, but I guess that dream wasn't meant to be like. I guess Peter Pan was right, and I don't care if I never land. Cus' the distant sky's always better than my life right now.",
+    "original": "A couple kids, one girl and a sailor, met a boy in green. I thought it'd be me, but I guess that dream wasn't meant to be like. I guess Peter Pan was right, and I don't care if I never land. Cus' the distant sky's always better than my life right now.",
+    "translation": "几个孩子，一个女孩和一个水手，遇到了一个穿绿色衣服的男孩。我以为会是我，但我觉得那个梦并不是注定要这样的。我觉得彼得·潘是对的，我不在乎我是否永远无法着陆。因为遥远的天空总是比我现在的生活在得好。",
+    "phonetic": "[ə ˈkʌpl kɪdz, wʌn ɡɜ˂rl ənd ə ˈseɪlər, mɛt ə bɔɪ ɪn ɡri˂n, aɪ θɔ˂t ɪtdə bi˂ mi˂, bʌt aɪ ɡɛs ðæt dri˂m ˈwɒzənt mɛnt tu˂ bi˂ laɪk, aɪ ɡɛs pɪˈtər pæn wɒz raɪt, ənd aɪ doʂnt kɛər ɪf aɪ ˈnɛvər lænd, kəz ðə ˈdɪstənt skaɪz ˈɔ˂lweɪz ˈbɛtər ðən maɪ laɪf raɪt naʂ]"
   },
   {
     "sentence": "So for one last time, I guess Peter Pan was right.",
     "original": "So for one last time, I guess Peter Pan was right.",
     "translation": "所以最后再说一遍，我觉得彼得·潘是对的。",
-    "phonetic": "[soʊ fɔːr wʌn lɑːst taɪm, aɪ ɡɛs pɪˈtər pæn wɒz raɪt]"
+    "phonetic": "[soʂ fɔ˂r wʌn lɑ˂st taɪm, aɪ ɡɛs pɪˈtər pæn wɒz raɪt]"
   }
 ]
 
-const list4 = [
-    {
-      "sentence": "Growing up's a waste of time, so I think I'll fly away.",
-      "original": "Growing up's a waste of time, so I think I'll fly away.",
-      "translation": "长大成人就是浪费时间，所以我觉得我会飞走。",
-      "phonetic": "[ˈɡroʊɪŋ ʌpːz ə weɪst əv taɪm, soʊ aɪ θɪŋk aɪl flaɪ əˈweɪ]"
-    },
-    {
-      "sentence": "Set a course for brighter days, find the second star, I'm soaring.",
-      "original": "Set a course for brighter days, find the second star, I'm soaring.",
-      "translation": "设定航向更光明的日子，找到第二颗星，我在翱翔。",
-      "phonetic": "[sɛt ə kɔːrs fɔːr ˈbraɪtər deɪz, faɪnd ðə sɛkənd stɑːr, aɪm ˈsɔːrɪŋ]"
-    },
-    {
-      "sentence": "Then straight on to the morning, I know that I'll be fine.",
-      "original": "Then straight on to the morning, I know that I'll be fine.",
-      "translation": "然后直飞向早晨，我知道我会好好的。",
-      "phonetic": "[ðɛn streɪt ɑːn tuː ðə ˈmɔːrnɪŋ, aɪ noʊ ðæt aɪl biː faɪn]"
-    },
-    {
-      "sentence": "‘Cus I know Peter Pan was right, and I don't care.",
-      "original": "'Cus I know Peter Pan was right, and I don't care.",
-      "translation": "因为我知道彼得·潘是对的，我不在乎。",
-      "phonetic": "[kəz aɪ noʊ ˈpɪtər pæn wɒz raɪt, ənd aɪ doʊnt kɛər]"
-    },
-    {
-      "sentence": "If I never land, the distant sky's always better than my life right now.",
-      "original": "If I never land, the distant sky's always better than my life right now.",
-      "translation": "如果我永远不着陆，遥远的天空总比我现在的生活要好。",
-      "phonetic": "[ɪf aɪ ˈnɛvər lænd, ðə ˈdɪstənt skaɪz ˈɔːlweɪz ˈbɛtər ðən maɪ laɪf raɪt naʊ]"
-    },
-    {
-      "sentence": "And the place I am, so for one last time, I guess Peter Pan was right.",
-      "original": "And the place I am, so for one last time, I guess Peter Pan was right.",
-      "translation": "以及我所在的地方，所以最后再说一遍，我觉得彼得·潘是对的。",
-      "phonetic": "[ænd ðə pleɪs aɪ æm, soʊ fɔːr wʌn lɑːst taɪm, aɪ ɡɛs ˈpɪtər pæn wɒz raɪt]"
-    }
-  ]
+export default list1;
 
-  function removeDuplicates(...lists: { sentence: string; original: string; translation: string; phonetic: string; }[][]) {
-    const uniqueSentences = new Map();
-  
-    lists.forEach(list => {
-      list.forEach(item => {
-        const originalSentence = item.original;
-        if (!uniqueSentences.has(originalSentence)) {
-          uniqueSentences.set(originalSentence, item);
-        }
-      });
-    });
-  
-    return Array.from(uniqueSentences.values());
-  }
-  
-  const combinedList = removeDuplicates(list1, list2, list3, list4);
-  
-  export default combinedList;
+ 
 
 
 
